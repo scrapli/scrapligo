ade6feb3ca8041aa82af2ffc8717877de26b99cb
Carl Montanari <carl.r.montanari@gmail.com>
2021-11-24 16:24:21 -0800

    timeouts, new, options

--- driver/base/readcallback.go
+++ driver/base/readcallback.go
@@ -8,7 +8,126 @@ import (
 	"time"
 )
 
+var ErrMustSetContains = errors.New("must set Contains or ContainsRe")
 var ErrCallbackAlreadyTriggered = errors.New("callback set to 'OnlyOnce', but already triggered")
+var ErrCallbackTimeout = errors.New("callback timeout")
+
+type ReadCallbackOption func(callback *ReadCallback) error
+
+func WithCallbackContains(contains string) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.Contains = contains
+
+		return nil
+	}
+}
+
+func WithCallbackContainsRe(contains string) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.ContainsRe = contains
+
+		return nil
+	}
+}
+
+func WithCallbackCaseInsensitive(i bool) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.CaseInsensitive = i
+
+		return nil
+	}
+}
+
+func WithCallbackMultiline(m bool) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.MultiLine = m
+
+		return nil
+	}
+}
+
+func WithCallbackResetOutput(reset bool) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.ResetOutput = reset
+
+		return nil
+	}
+}
+
+func WithCallbackOnlyOnce(o bool) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.OnlyOnce = o
+
+		return nil
+	}
+}
+
+func WithCallbackNextTimeout(t time.Duration) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.NextTimeout = t
+
+		return nil
+	}
+}
+
+func WithCallbackNextReadDelay(t time.Duration) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.NextReadDelay = t
+
+		return nil
+	}
+}
+
+func WithCallbackComplete(complete bool) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.Complete = complete
+
+		return nil
+	}
+}
+
+func WithCallbackName(name string) ReadCallbackOption {
+	return func(r *ReadCallback) error {
+		r.Name = name
+
+		return nil
+	}
+}
+
+func NewReadCallback(
+	callback func(*Driver, string) error,
+	options ...ReadCallbackOption,
+) (*ReadCallback, error) {
+	rc := &ReadCallback{
+		Callback:           callback,
+		Contains:           "",
+		containsBytes:      nil,
+		ContainsRe:         "",
+		containsReCompiled: nil,
+		CaseInsensitive:    true,
+		MultiLine:          true,
+		ResetOutput:        true,
+		OnlyOnce:           false,
+		NextTimeout:        0,
+		NextReadDelay:      0,
+		triggered:          false,
+		Complete:           false,
+		Name:               "",
+	}
+
+	for _, option := range options {
+		err := option(rc)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if rc.Contains == "" && rc.ContainsRe == "" {
+		return nil, ErrMustSetContains
+	}
+
+	return rc, nil
+}
 
 type ReadCallback struct {
 	Callback           func(*Driver, string) error
@@ -21,10 +140,14 @@ type ReadCallback struct {
 	// ResetOutput bool indicating if the output should be reset or not after callback execution.
 	ResetOutput bool
 	// OnlyOnce bool indicating if this callback should be executed only one time.
-	OnlyOnce  bool
-	triggered bool
-	Complete  bool
-	Name      string
+	OnlyOnce bool
+	// NextTimout timeout value to use for the subsequent read loop - ignored if Complete is true.
+	NextTimeout time.Duration
+	// NextReadDelay is time to use for sleeps between reads for hte subsequent read loop.
+	NextReadDelay time.Duration
+	triggered     bool
+	Complete      bool
+	Name          string
 }
 
 func (r *ReadCallback) contains() []byte {
@@ -53,10 +176,18 @@ func (r *ReadCallback) containsRe() *regexp.Regexp {
 	return r.containsReCompiled
 }
 
+type readCallbackResult struct {
+	i         int
+	callbacks []*ReadCallback
+	output    []byte
+	err       error
+}
+
 func (d *Driver) executeCallback(
 	i int,
 	callbacks []*ReadCallback,
 	output []byte,
+	timeout,
 	readDelay time.Duration) error {
 	callback := callbacks[i]
 
@@ -81,41 +212,83 @@ func (d *Driver) executeCallback(
 		output = []byte{}
 	}
 
-	return d.readWithCallbacks(callbacks, output, readDelay)
+	nextTimeout := timeout
+	if callback.NextTimeout != 0 {
+		nextTimeout = callback.NextTimeout
+	}
+
+	nextReadDelay := readDelay
+	if callback.NextReadDelay != 0 {
+		nextReadDelay = callback.NextReadDelay
+	}
+
+	return d.readWithCallbacks(callbacks, output, nextTimeout, nextReadDelay)
 }
 
 func (d *Driver) readWithCallbacks(
 	callbacks []*ReadCallback,
 	output []byte,
+	timeout,
 	readDelay time.Duration,
 ) error {
-	for {
-		newOutput, err := d.Channel.Read()
-		if err != nil {
-			return err
-		}
+	c := make(chan *readCallbackResult)
 
-		output = append(output, newOutput...)
+	go func() {
+		defer close(c)
 
-		for i, callback := range callbacks {
-			o := output
-			if callback.CaseInsensitive {
-				o = bytes.ToLower(output)
+		for {
+			newOutput, err := d.Channel.Read()
+			if err != nil {
+				c <- &readCallbackResult{
+					err: err,
+				}
+
+				return
 			}
 
-			if (callback.Contains != "" && bytes.Contains(o, callback.contains())) ||
-				(callback.ContainsRe != "" && callback.containsRe().Match(o)) {
-				return d.executeCallback(i, callbacks, output, readDelay)
+			output = append(output, newOutput...)
+
+			for i, callback := range callbacks {
+				o := output
+				if callback.CaseInsensitive {
+					o = bytes.ToLower(output)
+				}
+
+				if (callback.Contains != "" && bytes.Contains(o, callback.contains())) ||
+					(callback.ContainsRe != "" && callback.containsRe().Match(o)) {
+					c <- &readCallbackResult{
+						i:         i,
+						callbacks: callbacks,
+						output:    output,
+						err:       nil,
+					}
+
+					return
+				}
 			}
+
+			time.Sleep(readDelay)
+		}
+	}()
+
+	timer := time.NewTimer(timeout)
+
+	select {
+	case r := <-c:
+		if r.err != nil {
+			return r.err
 		}
 
-		time.Sleep(readDelay)
+		return d.executeCallback(r.i, r.callbacks, r.output, timeout, readDelay)
+	case <-timer.C:
+		return ErrCallbackTimeout
 	}
 }
 
 func (d *Driver) ReadWithCallbacks(
 	callbacks []*ReadCallback,
 	input string,
+	timeout,
 	readDelay time.Duration,
 ) error {
 	if input != "" {
@@ -125,5 +298,12 @@ func (d *Driver) ReadWithCallbacks(
 		}
 	}
 
-	return d.readWithCallbacks(callbacks, []byte{}, readDelay)
+	origTransportTimeout := d.Transport.BaseTransportArgs.TimeoutTransport
+	d.Transport.BaseTransportArgs.TimeoutTransport = 0
+
+	r := d.readWithCallbacks(callbacks, []byte{}, timeout, readDelay)
+
+	d.Transport.BaseTransportArgs.TimeoutTransport = origTransportTimeout
+
+	return r
 }
