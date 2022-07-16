4b4819acf7cfba5b3189f13e03c0ddc63038b981
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2022-07-17 08:26:30 +1000

    Add Netconf WithFilter

--- driver/opoptions/netconf_test.go
+++ driver/opoptions/netconf_test.go
@@ -137,3 +137,66 @@ func TestWithDefaultType(t *testing.T) {
 		t.Run(testName, f)
 	}
 }
+
+func testWithFilter(testName string, testCase *struct {
+	description string
+	s           string
+	o           interface{}
+	isignored   bool
+},
+) func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Logf("%s: starting", testName)
+
+		err := opoptions.WithFilter(testCase.s)(testCase.o)
+		if err != nil {
+			if errors.Is(err, util.ErrIgnoredOption) && !testCase.isignored {
+				t.Fatalf(
+					"%s: option should be ignored, but returned different error",
+					testName,
+				)
+			}
+
+			return
+		}
+
+		oo, _ := testCase.o.(*netconf.OperationOptions)
+
+		if !cmp.Equal(oo.Filter, testCase.s) {
+			t.Fatalf(
+				"%s: actual and expected filter do not match\nactual: %v\nexpected:%v",
+				testName,
+				oo.Filter,
+				testCase.s,
+			)
+		}
+	}
+}
+
+func TestWithFilter(t *testing.T) {
+	cases := map[string]*struct {
+		description string
+		s           string
+		o           interface{}
+		isignored   bool
+	}{
+		"set-filter": {
+			description: "simple set option test",
+			s:           "potato",
+			o:           &netconf.OperationOptions{},
+			isignored:   false,
+		},
+		"ignored": {
+			description: "skipped due to ignored type",
+			s:           "xpath",
+			o:           &network.Driver{},
+			isignored:   true,
+		},
+	}
+
+	for testName, testCase := range cases {
+		f := testWithFilter(testName, testCase)
+
+		t.Run(testName, f)
+	}
+}
