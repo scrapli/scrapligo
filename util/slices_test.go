f922f4ae284d8f2b80c743dfb34a2fb393a8d475
Marcus Hines <marcus.hines@gmail.com>
2022-07-28 19:32:42 -0700

    refactor slices_test to match go style

--- util/slices_test.go
+++ util/slices_test.go
@@ -1,46 +1,31 @@
-package util_test
+package util
 
 import (
 	"testing"
-
-	"github.com/scrapli/scrapligo/util"
 )
 
-func testStringSliceContains(testName, s string, ss []string, expected bool) func(t *testing.T) {
-	return func(t *testing.T) {
-		t.Logf("%s: starting", testName)
-
-		if util.StringSliceContains(ss, s) != expected {
-			t.Fatalf(
-				"%s: actual and expected results differ",
-				testName,
-			)
-		}
-	}
-}
-
 func TestStringSliceContains(t *testing.T) {
-	cases := map[string]struct {
-		description string
-		ss          []string
-		s           string
-		expected    bool
-	}{
-		"string-slice-contains-simple-true": {
-			ss:       []string{"taco", "cat", "race", "car"},
-			s:        "taco",
-			expected: true,
-		},
-		"string-slice-contains-simple-false": {
-			ss:       []string{"taco", "cat", "race", "car"},
-			s:        "potato",
-			expected: false,
-		},
-	}
-
-	for testName, testCase := range cases {
-		f := testStringSliceContains(testName, testCase.s, testCase.ss, testCase.expected)
-
-		t.Run(testName, f)
+	tests := []struct {
+		desc string
+		ss   []string
+		s    string
+		want bool
+	}{{
+		desc: "string-slice-contains-simple-true",
+		ss:   []string{"taco", "cat", "race", "car"},
+		s:    "taco",
+		want: true,
+	}, {
+		desc: "string-slice-contains-simple-false",
+		ss:   []string{"taco", "cat", "race", "car"},
+		s:    "potato",
+		want: false,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			if got := StringSliceContains(tt.ss, tt.s); got != tt.want {
+				t.Fatalf("StringSliceContains(%v, %q) failed: got %v, want %v", tt.ss, tt.s, got, tt.want)
+			}
+		})
 	}
 }
