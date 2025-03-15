788b2f8932a2d65e9ce3d1b3bca47157917c1e2e
Carl Montanari <carl.r.montanari@gmail.com>
2025-03-15 16:12:41 -0700

    test: fix textfsm parse test

--- util/parse_test.go
+++ util/parse_test.go
@@ -22,16 +22,26 @@ func TestTextFsmParse(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Logf("%s: starting", testName)
 
+			testFixturePath, err := filepath.Abs(fmt.Sprintf("./fixtures/%s", testName))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			testTemplateFixturePath, err := filepath.Abs(
+				"./fixtures/cisco_ios_show_version.textfsm",
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+
 			testGoldenPath, err := filepath.Abs(fmt.Sprintf("./golden/%s", testName))
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			s := string(scrapligotesthelper.ReadFile(t, fmt.Sprintf("%s.txt", testName)))
-
 			actualOut, err := scrapligoutil.TextFsmParse(
-				s,
-				"test-fixtures/cisco_ios_show_version.textfsm",
+				string(scrapligotesthelper.ReadFile(t, testFixturePath)),
+				testTemplateFixturePath,
 			)
 			if err != nil {
 				t.Fatalf("%s: encountered error parsing with textfsm, error: %s", testName, err)
@@ -47,20 +57,16 @@ func TestTextFsmParse(t *testing.T) {
 			}
 
 			if *scrapligotesthelper.Update {
-				scrapligotesthelper.WriteFile(t, testGoldenPath, actualOutJSON)
-			} else {
-				testGoldenContent := scrapligotesthelper.ReadFile(
+				scrapligotesthelper.WriteFile(
 					t,
-					fmt.Sprintf("golden/%s-out.txt", testName),
+					testGoldenPath,
+					actualOutJSON,
 				)
+			} else {
+				testGoldenContent := scrapligotesthelper.ReadFile(t, testGoldenPath)
 
 				if !bytes.Equal(actualOutJSON, testGoldenContent) {
-					t.Fatalf(
-						"%s: actual and expected outputs do not match\nactual: %s\nexpected:%s",
-						testName,
-						actualOutJSON,
-						testGoldenContent,
-					)
+					scrapligotesthelper.FailOutput(t, actualOutJSON, testGoldenContent)
 				}
 			}
 		})
