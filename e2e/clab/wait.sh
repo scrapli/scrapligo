a1a8b24daa490cef17de95c85fc203e958b07e01
Carl Montanari <carl.r.montanari@gmail.com>
2025-03-16 19:17:10 -0700

    chore: fix wait script for srl node

--- e2e/clab/wait.sh
+++ e2e/clab/wait.sh
@@ -4,6 +4,7 @@ INTERVAL=5
 END_TIME=$((SECONDS + MAX_TIME))
 
 while [ $SECONDS -lt $END_TIME ]; do
+    echo "waiting for ssh..."
     # ensure srl is up and running and displays the normal banner (is past the initial boot stuff
     # basically where it is only basic cli)
     sshpass -p "NokiaSrl1!" \
@@ -11,6 +12,7 @@ while [ $SECONDS -lt $END_TIME ]; do
       | grep "Welcome to Nokia SR Linux"
 
     if [ $? -eq 0 ]; then
+        echo "ssh available..."
         break
     fi
 
@@ -18,12 +20,14 @@ while [ $SECONDS -lt $END_TIME ]; do
 done
 
 while [ $SECONDS -lt $END_TIME ]; do
+    echo "waiting for netconf..."
     # same thing for netconf port just to be safe!
     sshpass -p "NokiaSrl1!" \
-      ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -l admin -p 21830 172.20.20.16 quit  2>&1 \
+      ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -l admin -p 830 172.20.20.16 quit  2>&1 \
       | grep "Welcome to Nokia SR Linux"
 
     if [ $? -eq 0 ]; then
+        echo "netconf available..."
         exit 0
     fi
 
