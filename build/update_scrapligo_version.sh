1eb2c34e09c74afd3e5ea336617a00f9d37acecf
Carl Montanari <carl.r.montanari@gmail.com>
2025-05-18 18:32:46 -0700

    chore: install libscrapli in ci via build go script, update-y scripts

---
+++ build/update_scrapligo_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRAPLIGO_VERSION="${1:-}"
+
+if [[ -z "$SCRAPLIGO_VERSION" ]]; then
+    echo "error: scrapligo version must be set"
+    exit 1
+fi
+
+sed -i.bak -E "s|(var Version = )(.*)|\1${SCRAPLIGO_VERSION}\2|g" scrapli/__init__.py
