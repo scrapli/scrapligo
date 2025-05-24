cb0585a761b2b5cd5df301cfa387030644b41ba6
Carl Montanari <carl.r.montanari@gmail.com>
2025-05-24 11:27:29 -0700

    test: add send prompted input e2e test

--- e2e/cli/send_prompted_inputs_test.go
+++ e2e/cli/send_prompted_inputs_test.go
@@ -1,1 +1,77 @@
 package cli_test
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	scrapligocli "github.com/scrapli/scrapligo/cli"
+)
+
+func TestSendPromptedInput(t *testing.T) {
+	parentName := "send-prompted-input"
+
+	cases := map[string]struct {
+		description string
+		platform    string
+		transports  []string
+		input       string
+		prompt      string
+		response    string
+		options     []scrapligocli.OperationOption
+	}{
+		"eos-simple": {
+			description: "simple input that requires no pagination",
+			platform:    scrapligocli.AristaEos.String(),
+			transports:  []string{"bin", "ssh2", "telnet"},
+			input:       "read -p \"Will you prompt me plz? \" answer",
+			prompt:      "Will you prompt me plz?",
+			response:    "nou",
+			options: []scrapligocli.OperationOption{
+				scrapligocli.WithRequestedMode("bash"),
+			},
+		},
+	}
+
+	for caseName, caseData := range cases {
+		for _, transportName := range caseData.transports {
+			testName := fmt.Sprintf("%s-%s-%s", parentName, caseName, transportName)
+
+			t.Run(testName, func(t *testing.T) {
+				t.Logf("%s: starting", testName)
+
+				testGoldenPath, err := filepath.Abs(fmt.Sprintf("./golden/%s", testName))
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+				defer cancel()
+
+				c := getCli(t, caseData.platform, transportName)
+				defer func() {
+					_, _ = c.Close(ctx)
+				}()
+
+				_, err = c.Open(ctx)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				r, err := c.SendPromptedInput(
+					ctx,
+					caseData.input,
+					caseData.prompt,
+					caseData.response,
+					caseData.options...)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				assertResult(t, r, testGoldenPath)
+			})
+		}
+	}
+}
