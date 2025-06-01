77e18d8a78bed6828cd4a70bb38a1d2f2fd0336b
Carl Montanari <carl.r.montanari@gmail.com>
2025-06-01 08:44:57 -0700

    chore: use scrapli_clab for ci unit tests

--- build/write_libscrapli_to_cache.go
+++ build/write_libscrapli_to_cache.go
@@ -6,6 +6,8 @@ import (
 	scrapligoffi "github.com/scrapli/scrapligo/ffi"
 )
 
+// just a simple program to easily expose the EnsureLibscrapli function for devs. obv just
+// `go run build/write_libscrapli_to_cache.go`
 func main() {
 	p, err := scrapligoffi.EnsureLibscrapli()
 	if err != nil {
