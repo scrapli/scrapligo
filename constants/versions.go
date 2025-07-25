66547950749c3c5d1becbabb0e6714a2557c89c5
Carl Montanari <carl.r.montanari@gmail.com>
2025-07-25 06:27:25 -0700

    chore: repin definitions

--- constants/versions.go
+++ constants/versions.go
@@ -5,8 +5,8 @@ var Version = "2.0.0-alpha.1"
 
 // LibScrapliVersion is the version of libscrapli scrapligo was built with. Should be set prior to
 // a release via build/update_all.sh to the version of libscrapli bundled in assets.
-var LibScrapliVersion = "0.0.1-beta.4"
+var LibScrapliVersion = "0.0.1-beta.5"
 
 // ScrapliDefinitionsVersion is the version of scrapli definitions embedded in assets in this build.
 // This should be set prior to a release via build/update_all.sh.
-var ScrapliDefinitionsVersion = "c38ec6d"
+var ScrapliDefinitionsVersion = "1a3d771"
