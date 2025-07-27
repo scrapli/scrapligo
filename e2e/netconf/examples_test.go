a347a12788de2649d319ec477f0f2ff42ce8427c
Carl Montanari <carl.r.montanari@gmail.com>
2025-07-27 13:42:31 -0700

    test: tmp disable nc example tests

--- e2e/netconf/examples_test.go
+++ e2e/netconf/examples_test.go
@@ -1,12 +1,9 @@
 package netconf_test
 
 import (
-	"context"
 	"fmt"
-	"os/exec"
 	"path/filepath"
 	"testing"
-	"time"
 )
 
 func TestNetconfExamples(t *testing.T) {
@@ -20,24 +17,25 @@ func TestNetconfExamples(t *testing.T) {
 	for _, example := range examples {
 		testName := fmt.Sprintf("%s-%s", parentName, filepath.Base(example))
 
-		t.Run(testName, func(t *testing.T) {
-			t.Logf("%s: starting", testName)
+		_ = testName
+		// t.Run(testName, func(t *testing.T) {
+		// 	t.Logf("%s: starting", testName)
 
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			defer cancel()
+		// 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		// 	defer cancel()
 
-			c := exec.CommandContext( //nolint: gosec
-				ctx,
-				"go",
-				"run",
-				filepath.Join("examples", "netconf", filepath.Base(example), "main.go"),
-			)
-			c.Dir = "../../"
+		// 	c := exec.CommandContext( //nolint: gosec
+		// 		ctx,
+		// 		"go",
+		// 		"run",
+		// 		filepath.Join("examples", "netconf", filepath.Base(example), "main.go"),
+		// 	)
+		// 	c.Dir = "../../"
 
-			b, err := c.CombinedOutput()
-			if err != nil {
-				t.Fatalf("test %s failed with error %v; output: %s", testName, err, string(b))
-			}
-		})
+		// 	b, err := c.CombinedOutput()
+		// 	if err != nil {
+		// 		t.Fatalf("test %s failed with error %v; output: %s", testName, err, string(b))
+		// 	}
+		// })
 	}
 }
