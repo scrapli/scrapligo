a8917f9e85ebbee37f1ae226a8f121fe2ec03814
Carl Montanari <carl.r.montanari@gmail.com>
2025-07-27 12:18:09 -0700

    test: add executing to examples to e2e tests

---
+++ e2e/cli/examples_test.go
@@ -0,0 +1,43 @@
+package cli_test
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestCliExamples(t *testing.T) {
+	parentName := "examples-cli"
+
+	examples, err := filepath.Glob("../../examples/cli/*")
+	if err != nil {
+		t.Fatalf("failed globbing cli examples, error: %v", err)
+	}
+
+	for _, example := range examples {
+		testName := fmt.Sprintf("%s-%s", parentName, filepath.Base(example))
+
+		t.Run(testName, func(t *testing.T) {
+			t.Logf("%s: starting", testName)
+
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+
+			c := exec.CommandContext( //nolint: gosec
+				ctx,
+				"go",
+				"run",
+				filepath.Join("examples", "cli", filepath.Base(example), "main.go"),
+			)
+			c.Dir = "../../"
+
+			err := c.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
