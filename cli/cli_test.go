f55292f1e26e59ea6537769cc9fcc4c9c54c4aae
Carl Montanari <carl.r.montanari@gmail.com>
2026-07-25 17:50:57 -0700

    test: crank up concurrency test a bit more now that the ssh2 bit is fixed

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -118,7 +118,7 @@ func TestConcurrency(t *testing.T) { //nolint: gocognit
 				)
 			}
 
-			for range 200 {
+			for range 500 {
 				wg.Go(
 					func() {
 						// tiny sleep seems to make the test way more consistent -- at least locally
